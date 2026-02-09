c7c81dc7a8f135b8b8b251c85a08e0e6d1d9b8c4
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2026-02-09 18:42:58 +0000

    more commands

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+if !$(ls $HOME/.profile); then
+	mkdir -p $HOME/.profile
+
 echo 'export PATH="$HOME/.local/bin${PATH+:$PATH}"' >> ~/.profile
 
 source ~/.profile
@@ -8,4 +11,22 @@ tar -xzf ripgrep.tar.gz
 
 rm ripgrep.tar.gz
 
-mv ripgrep-15.1.0-x86_64-unknown-linux-musl/rg .local/bin/
+mv ripgrep-15.1.0-x86_64-unknown-linux-musl/rg $HOME/.local/bin/
+
+rm -rf ripgrep-15.1.0-x86_64-unknown-linux-musl
+
+curl -L -o fd-find.tar.gz --progress-bar https://github.com/sharkdp/fd/releases/download/v10.3.0/fd-v10.3.0-x86_64-unknown-linux-gnu.tar.gz
+
+tar -xzf fd-find.tar.gz
+
+rm fd-find.tar.gz
+
+mv fd-v10.3.0-x86_64-unknown-linux-gnu/fd $HOME/.local/bin/
+
+rm -rf fd-v10.3.0-x86_64-unknown-linux-gnu
+
+curl -L -o neovim.tar.gz https://github.com/neovim/neovim/releases/download/v0.11.6/nvim-linux-x86_64.tar.gz
+
+tar -xzvf neovim.tar.gz
+
+rm neovim.tar.gz
