81534e50aae46af6d8f69b3004a29e2097f8306a
Disvster <mmcouto2001@gmail.com>
2026-02-09 18:17:21 +0000

    WIP install script

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
+echo 'export PATH="$HOME/.local/bin${PATH+:$PATH}"' >> ~/.profile
+
+source ~/.profile
+
 curl -L -o ripgrep.tar.gz --progress-bar https://github.com/BurntSushi/ripgrep/releases/download/15.1.0/ripgrep-15.1.0-x86_64-unknown-linux-musl.tar.gz
+
+tar -xzf ripgrep.tar.gz
+
+rm ripgrep.tar.gz
+
+mv ripgrep-15.1.0-x86_64-unknown-linux-musl/rg .local/bin/
